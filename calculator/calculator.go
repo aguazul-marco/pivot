f8924c85ea430d5f72577846eb194f0ea85cf49a
aguazul-marco <marco@shipt.com>
2022-11-02 08:04:52 -0500

    Packages init

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,1 +1,17 @@
-package main
+package calculator
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Subtract(a, b int) int {
+	return a - b
+}
+
+func Multiply(a, b int) int {
+	return a * b
+}
+
+//func Divide(a, b int) (int, err) {
+//	return a - b
+//}
--- cmd/cal/main.go
+++ cmd/cal/main.go
@@ -1,1 +1,17 @@
-package cal
+package main
+
+import (
+	"fmt"
+	"github.com/aguazul-marco/pivot/calculator"
+)
+
+func main() {
+
+	addResult := calculator.Add(8, 8)
+	subResult := calculator.Subtract(8, 8)
+	multiplyResult := calculator.Multiply(8, 8)
+
+	fmt.Println(addResult)
+	fmt.Println(subResult)
+	fmt.Println(multiplyResult)
+}
