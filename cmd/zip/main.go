19e6e210e3d271a6f7c8b54946b1c44523d23d1c
aguazul-marco <marco@shipt.com>
2022-11-15 22:15:52 -0600

    Updated example

--- cmd/zip/main.go
+++ cmd/zip/main.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 	"log"
 
-	// "github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -17,23 +17,63 @@ func main() {
 	}
 	defer db.Close()
 
+	if err := seed(db); err != nil {
+		log.Fatal(err)
+	}
+
 	rows, err := db.Query("SELECT id, code, city, state FROM places")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
+	type place struct {
+		id    int
+		code  string
+		city  string
+		state string
+	}
+
+	var places []place
+
 	for rows.Next() {
-		var id int
-		var code, city, state string
-		err = rows.Scan(&id, &code, &city, &state)
+		var p place
+		err = rows.Scan(&p.id, &p.code, &p.city, &p.state)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(id, code, city, state)
+		places = append(places, p)
 	}
 	err = rows.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	spew.Dump(places)
+}
+
+func seed(db *sql.DB) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+	stmt, err := tx.Prepare("INSERT INTO places(code, city, state) VALUES(?, ?, ?)")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for i := 0; i < 5; i++ {
+		_, err := stmt.Exec(fmt.Sprintf("code-%d", i), fmt.Sprintf("city-%d", i), fmt.Sprintf("state-%d", i))
+		if err != nil {
+			return err
+		}
+		// spew.Dump(r)
+	}
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
